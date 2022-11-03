8528763759d6cbb9d7ac23f4a2442ca9d39e4897
haresh2349 <hareshcsolanki07@gmail.com>
2022-11-03 18:00:30 +0530

    code fixed

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const UserSchema = new Schema({
   email: { type: String, required: true },
-  mobile: { type: Number, required: true },
+  mobile: { type: Number },
   full_name: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
